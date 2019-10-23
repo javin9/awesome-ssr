d9531fbd4753d1de1822e72eff6363f043d4d1f6
cupid <liujianwei1213@163.com>
2019-10-23 15:22:40 +0800

    feat: webpack完整版配置

--- vue.config.js
+++ vue.config.js
@@ -19,20 +19,24 @@ module.exports = {
     // 这允许 webpack 以 Node 适用方式处理动态导入(dynamic import)，
     // 并且还会在编译 Vue 组件时告知 `vue-loader` 输送面向服务器代码(server-oriented code)。
     target: TARGET_NODE ? 'node' : 'web',
-    node: TARGET_NODE ? undefined : false,
     output: {
       // 此处使用 Node 风格导出模块
       libraryTarget: TARGET_NODE ? 'commonjs2' : undefined
     },
-    externals: nodeExternals({
-      // 不要外置化 webpack 需要处理的依赖模块。
-      // 你可以在这里添加更多的文件类型。例如，未处理 *.vue 原始文件，
-      // 你还应该将修改 `global`（例如 polyfill）的依赖模块列入白名单
-      whitelist: /\.css$/
-    }),
-    // 这是将服务器的整个输出构建为单个 JSON 文件的插件。
     // 服务端默认文件名为 `vue-ssr-server-bundle.json`
     plugins: [TARGET_NODE ? new VueSSRServerPlugin() : new VueSSRClientPlugin()]
   }),
-  chainWebpack: config => { if (process.env.NODE_ENV === 'production') { config.optimization.delete('splitChunks') } }
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === 'production') { config.optimization.delete('splitChunks') }
+    // https://github.com/vuejs/vue-loader/issues/885
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        // 修改它的选项...
+        options['optimizeSSR'] = false
+        return options
+      })
+  }
 }
